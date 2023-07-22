fde51dc2199abb0e336dd7060114bbf14590a37e
Daniil <dadyarri@gmail.com>
2023-07-22 22:50:05 +0300

    add descriptions for tags

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -7,11 +7,19 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddFastEndpoints();
 builder.Services.AddDbContextPool<AppDbContext>(
     options => options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
-builder.Services.SwaggerDocument(o => o.DocumentSettings = s =>
+builder.Services.SwaggerDocument(o =>
 {
-    s.Title = "UNI API";
-    s.DocumentName = "Version 1";
-    s.Version = "v1";
+    o.AutoTagPathSegmentIndex = 0;
+    o.TagDescriptions = t =>
+    {
+        t["Roles"] = "API of users' roles (student, tutor, administrator)";
+    };
+    o.DocumentSettings = s =>
+    {
+        s.Title = "UNI API";
+        s.DocumentName = "Version 1";
+        s.Version = "v1";
+    };
 });
 
 var app = builder.Build();
