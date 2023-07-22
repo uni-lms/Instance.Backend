4d66123684e1ab383fb9e760c431174b2a103902
Daniil <dadyarri@gmail.com>
2023-07-22 17:05:35 +0300

    add course model

---
+++ Backend/Modules/Courses/Contract/Course.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Data;
+using Backend.Modules.Users.Contract;
+
+namespace Backend.Modules.Courses.Contract;
+
+public class Course: BaseModel
+{
+    public string Name { get; set; }
+    public string Abbreviation { get; set; }
+    public IEnumerable<Group> AssignedGroups { get; set; }
+    public int Semester { get; set; }
+    public User Owner { get; set; }
+}
