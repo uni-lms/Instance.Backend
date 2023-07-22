c95ecfc869256798b0a3b21f20273711d1b3895b
Daniil <dadyarri@gmail.com>
2023-07-22 22:55:03 +0300

    add endpoint to fetch all roles

---
+++ Backend/Modules/Roles/Documentation/GetAllRolesDocs.cs
@@ -0,0 +1,15 @@
+﻿using Backend.Modules.Roles.Contract;
+using Backend.Modules.Roles.Endpoints;
+using FastEndpoints;
+
+namespace Backend.Modules.Roles.Documentation;
+
+public class GetAllRolesDocs : Summary<GetAllRoles>
+{
+    public GetAllRolesDocs()
+    {
+        Summary = "Gets all available roles";
+        Response(200, "Roles fetched successfully",
+            example: new List<Role> { new() { Id = Guid.NewGuid(), Name = "Tutors" } });
+    }
+}
