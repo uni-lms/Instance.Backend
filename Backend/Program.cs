ee80e78887ec35f166d6dede49bb4642d9526fda
Daniil <dadyarri@gmail.com>
2023-07-22 16:18:06 +0300

    add fastendpoints and enable swagger generation

--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,6 +1,17 @@
-var builder = WebApplication.CreateBuilder(args);
-var app = builder.Build();
+global using FastEndpoints;
+global using FastEndpoints.Swagger;
 
-app.MapGet("/", () => "Hello World!");
+var builder = WebApplication.CreateBuilder(args);
+builder.Services.AddFastEndpoints();
+builder.Services.SwaggerDocument(o => o.DocumentSettings = s =>
+{
+    s.Title = "UNI API";
+    s.Version = "v1";
+});
+
+var app = builder.Build();
+app.UseAuthorization();
+app.UseFastEndpoints();
+app.UseSwaggerGen();
 
 app.Run();
