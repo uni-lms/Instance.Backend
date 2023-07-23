fe4838d47ac0bd26c0e5502d9ec448719d3dc27d
Daniil <dadyarri@gmail.com>
2023-07-23 23:58:22 +0300

    add model for creating user

---
+++ Backend/Modules/Users/Contract/CreateUserRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Modules.Users.Contract;
+
+public class CreateUserRequest
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public Guid RoleId { get; set; }
+}
