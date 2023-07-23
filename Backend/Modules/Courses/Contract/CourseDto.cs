3e474067c7832e394cb58f1e4b7df23c9be0e874
Daniil <dadyarri@gmail.com>
2023-07-23 09:50:18 +0300

    add dto and mapper for course

---
+++ Backend/Modules/Courses/Contract/CourseDto.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Modules.Groups.Contract;
+using Backend.Modules.Users.Contract;
+
+namespace Backend.Modules.Courses.Contract;
+
+public class CourseDto
+{
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Abbreviation { get; set; }
+    public required IEnumerable<GroupDto> AssignedGroups { get; set; }
+    public int Semester { get; set; }
+    public required UserDto Owner { get; set; }
+}
---
+++ Backend/Modules/Courses/Contract/CoursesMapper.cs
@@ -0,0 +1,24 @@
+﻿using Backend.Modules.Groups.Contract;
+using Backend.Modules.Users.Contract;
+using FastEndpoints;
+
+namespace Backend.Modules.Courses.Contract;
+
+public class CoursesMapper : ResponseMapper<CourseDto, Course>
+{
+    public override CourseDto FromEntity(Course r)
+    {
+        var groupMapper = Resolve<GroupMapper>();
+        var userMapper = Resolve<UserMapper>();
+        return new CourseDto
+        {
+            Id = r.Id,
+            Name = r.Name,
+            Abbreviation = r.Abbreviation,
+            AssignedGroups = r.AssignedGroups
+                .Select(g => groupMapper.FromEntity(g)),
+            Owner = userMapper.FromEntity(r.Owner),
+            Semester = r.Semester
+        };
+    }
+}
