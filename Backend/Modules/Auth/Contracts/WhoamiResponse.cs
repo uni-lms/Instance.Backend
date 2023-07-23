862fc0f133643f0b17e96323a825f184a23e5534
Daniil <dadyarri@gmail.com>
2023-07-23 15:50:29 +0300

    add whoami endpoint

---
+++ Backend/Modules/Auth/Contracts/WhoamiResponse.cs
@@ -0,0 +1,9 @@
+﻿using System.Security.Claims;
+
+namespace Backend.Modules.Auth.Contracts;
+
+public class WhoamiResponse
+{
+    public string Email { get; set; }
+    public string Role { get; set; }
+}
---
+++ Backend/Modules/Auth/Endpoints/Whoami.cs
@@ -0,0 +1,24 @@
+﻿using System.Security.Claims;
+using Backend.Modules.Auth.Contracts;
+using FastEndpoints;
+
+namespace Backend.Modules.Auth.Endpoints;
+
+public class Whoami : EndpointWithoutRequest<WhoamiResponse>
+{
+    public override void Configure()
+    {
+        Get("/auth/whoami");
+        Options(x => x.WithTags("Auth"));
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var result = new WhoamiResponse
+        {
+            Email = User.Identity?.Name ?? "",
+            Role = User.FindAll(ClaimTypes.Role).ToList()[0].Value
+        };
+        await SendAsync(result, cancellation: ct);
+    }
+}
