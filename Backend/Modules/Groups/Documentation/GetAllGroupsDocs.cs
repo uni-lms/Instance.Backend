3d6c69da856318758dc68dade59b61ff2d1181ba
Daniil <dadyarri@gmail.com>
2023-07-23 11:26:20 +0300

    add endpoint to get all groups

---
+++ Backend/Modules/Groups/Documentation/GetAllGroupsDocs.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Modules.Groups.Endpoints;
+using FastEndpoints;
+
+namespace Backend.Modules.Groups.Documentation;
+
+public class GetAllGroupsDocs: Summary<GetAllGroups>
+{
+    public GetAllGroupsDocs()
+    {
+        Summary = "Gets all available groups";
+        Response(200, "Groups fetched successfully");
+    }
+}
