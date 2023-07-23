8cf91ba89854247721f89f68dfe36c9333713819
Daniil <dadyarri@gmail.com>
2023-07-23 14:23:53 +0300

    add generating random password and password's hash

--- Backend/Modules/Auth/Services/AuthService.cs
+++ Backend/Modules/Auth/Services/AuthService.cs
@@ -1,5 +1,6 @@
 ﻿using System.Security.Cryptography;
 using System.Text;
+using System.Text.Unicode;
 using Backend.Modules.Auth.Contracts;
 using Backend.Modules.Users.Contract;
 
@@ -12,10 +13,23 @@ public class AuthService
         return VerifyPasswordHash(req.Password, entity.PasswordHash, entity.PasswordSalt);
     }
 
+    public string GeneratePassword()
+    {
+        var passwordBytes = RandomNumberGenerator.GetBytes(13);
+        return Encoding.UTF8.GetString(passwordBytes);
+    }
+
     private bool VerifyPasswordHash(string plainPassword, byte[] passwordHash, byte[] passwordSalt)
     {
         using var hmac = new HMACSHA512(passwordSalt);
         var computedHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(plainPassword));
         return computedHash.SequenceEqual(passwordHash);
+    }
+
+    public void CreatePasswordHash(string plainPassword, out byte[] passwordSalt, out byte[] passwordHash)
+    {
+        using var hmac = new HMACSHA512();
+        passwordSalt = hmac.Key;
+        passwordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes(plainPassword));
     }
 }
