9c5a0a1aaffbdcb8ea66d2d75d8a4e8936224b22
Daniil <dadyarri@gmail.com>
2023-07-24 00:01:30 +0300

    add endpoint to create group

---
+++ Backend/Modules/Groups/Documentation/CreateGroupDocs.cs
@@ -0,0 +1,14 @@
+﻿using Backend.Modules.Groups.Endpoints;
+using FastEndpoints;
+
+namespace Backend.Modules.Groups.Documentation;
+
+public class CreateGroupDocs : Summary<CreateGroup>
+{
+    public CreateGroupDocs()
+    {
+        Summary = "Creates new group, adding users to it and sending emails with credentials to them";
+        Response(201, "Group created successfully");
+        Response(404, "Role was not found");
+    }
+}
---
+++ Backend/Modules/Groups/Endpoints/CreateGroup.cs
@@ -0,0 +1,79 @@
+﻿using System.Net.Mime;
+using Backend.Data;
+using Backend.Modules.Auth.Services;
+using Backend.Modules.Groups.Contract;
+using Backend.Modules.Users.Contract;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using Group = Backend.Modules.Groups.Contract.Group;
+
+namespace Backend.Modules.Groups.Endpoints;
+
+public class CreateGroup : Endpoint<CreateGroupRequest, GroupDto, GroupMapper>
+{
+    private readonly AppDbContext _db;
+    private readonly AuthService _authService;
+
+    public CreateGroup(AppDbContext db, AuthService authService)
+    {
+        _db = db;
+        _authService = authService;
+    }
+
+    public override void Configure()
+    {
+        Post("/groups");
+        Description(b => b
+            .Produces<GroupDto>(201, MediaTypeNames.Application.Json)
+            .ProducesProblemFE<InternalErrorResponse>(404)
+            .ProducesProblemFE<InternalErrorResponse>(500));
+        Options(x => x.WithTags("Groups"));
+        Version(1);
+        Roles("Administrator");
+    }
+
+    public override async Task HandleAsync(CreateGroupRequest req, CancellationToken ct)
+    {
+        var users = new List<User>();
+
+        var role = await _db.Roles
+            .Where(e => e.Name == "Student")
+            .FirstAsync(ct);
+
+        foreach (var reqUser in req.Users)
+        {
+            var password = _authService.GeneratePassword();
+            _authService.CreatePasswordHash(password, out var passwordSalt, out var passwordHash);
+
+            users.Add(new User
+            {
+                FirstName = reqUser.FirstName,
+                LastName = reqUser.LastName,
+                Email = reqUser.Email,
+                Role = role,
+                PasswordHash = passwordHash,
+                PasswordSalt = passwordSalt
+            });
+
+            // TODO: Добавить отправку писем с данными для входа (в очередь)
+        }
+
+        var group = new Group
+        {
+            Name = req.Name,
+            CurrentSemester = req.CurrentSemester,
+            MaxSemester = req.MaxSemester,
+            Students = users
+        };
+
+        await _db.Groups.AddAsync(group, ct);
+        await _db.SaveChangesAsync(ct);
+
+        await SendCreatedAtAsync(
+            "/groups",
+            null,
+            Map.FromEntity(group),
+            cancellation: ct
+        );
+    }
+}
