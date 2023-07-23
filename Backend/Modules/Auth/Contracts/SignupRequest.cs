b3523e612e66ddb11ff8c5bb2e7fbf176ab1ed07
Daniil <dadyarri@gmail.com>
2023-07-23 14:26:56 +0300

    add response-request models for signup

---
+++ Backend/Modules/Auth/Contracts/SignupRequest.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Modules.Auth.Contracts;
+
+public class SignupRequest
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public Guid Role { get; set; }
+}
---
+++ Backend/Modules/Auth/Contracts/SignupResponse.cs
@@ -0,0 +1,9 @@
+﻿namespace Backend.Modules.Auth.Contracts;
+
+public class SignupResponse
+{
+    public required string FirstName { get; set; }
+    public required string LastName { get; set; }
+    public required string Email { get; set; }
+    public required string Password { get; set; }
+}
