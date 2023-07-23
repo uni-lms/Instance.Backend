258480ab04ade2bf0c736c52b4153044a4432142
Daniil <dadyarri@gmail.com>
2023-07-23 23:58:39 +0300

    add model for creating group

---
+++ Backend/Modules/Groups/Contract/CreateGroupRequest.cs
@@ -0,0 +1,11 @@
+﻿using Backend.Modules.Users.Contract;
+
+namespace Backend.Modules.Groups.Contract;
+
+public class CreateGroupRequest
+{
+    public required string Name { get; set; }
+    public int CurrentSemester { get; set; }
+    public int MaxSemester { get; set; }
+    public required IEnumerable<CreateUserRequest> Users { get; set; }
+}
