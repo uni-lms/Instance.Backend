269c9191a88f25a90ad1a0427564dba208ac64ad
Daniil <dadyarri@gmail.com>
2023-07-23 09:49:26 +0300

    add dto and mapper for user

---
+++ Backend/Modules/Users/Contract/UserMapper.cs
@@ -0,0 +1,14 @@
+﻿using FastEndpoints;
+
+namespace Backend.Modules.Users.Contract;
+
+public class UserMapper: ResponseMapper<UserDto, User>
+{
+    public override UserDto FromEntity(User e) => new()
+    {
+        FirstName = e.FirstName,
+        LastName = e.LastName,
+        Email = e.Email,
+        RoleName = e.Role.Name
+    };
+}
