2f203568b0fe1e6ed194e05888ccc0839a6990a1
Daniil <dadyarri@gmail.com>
2023-07-23 09:20:32 +0300

    add endpoint to get all courses

---
+++ Backend/Modules/Courses/Documentation/GetAllCoursesDocs.cs
@@ -0,0 +1,13 @@
+﻿using Backend.Modules.Courses.Endpoints;
+using FastEndpoints;
+
+namespace Backend.Modules.Courses.Documentation;
+
+public class GetAllCoursesDocs: Summary<GetAllCourses>
+{
+    public GetAllCoursesDocs()
+    {
+        Summary = "Gets all available courses";
+        Response(200, "Courses fetched successfully");
+    }
+}
---
+++ Backend/Modules/Courses/Endpoints/GetAllCourses.cs
@@ -0,0 +1,34 @@
+﻿using System.Net.Mime;
+using Backend.Data;
+using Backend.Modules.Courses.Contract;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Modules.Courses.Endpoints;
+
+public class GetAllCourses : EndpointWithoutRequest<List<Course>>
+{
+    private readonly AppDbContext _db;
+
+    public GetAllCourses(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public override void Configure()
+    {
+        Get("/api/courses");
+        AllowAnonymous();
+        Description(b => b
+            .Produces<List<Course>>(200, MediaTypeNames.Application.Json)
+            .ProducesProblemFE<InternalErrorResponse>(500));
+        Options(x => x.WithTags("Courses"));
+        Version(1);
+    }
+
+    public override async Task HandleAsync(CancellationToken ct)
+    {
+        var result = await _db.Courses.ToListAsync(ct);
+        await SendAsync(result, cancellation: ct);
+    }
+}
