689e447f0b9a93f4bc8095b8bd680e3356ac6cda
Daniil <dadyarri@gmail.com>
2023-07-24 10:02:08 +0300

    refactor program class with extension methods

---
+++ Backend/Extensions/BuilderExtensions.cs
@@ -0,0 +1,73 @@
+﻿using Backend.Configuration;
+using Backend.Data;
+using Backend.Modules.Auth.Services;
+using FastEndpoints;
+using FastEndpoints.Security;
+using FastEndpoints.Swagger;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Options;
+using NSwag.Generation.AspNetCore;
+
+namespace Backend.Extensions;
+
+public static class BuilderExtensions
+{
+    public static void ConfigureFastEndpoints(this WebApplicationBuilder builder)
+    {
+        var signingKey = builder
+            .Configuration
+            .GetRequiredSection("Security")
+            .GetRequiredSection("SigningKey").Value;
+
+        ArgumentNullException.ThrowIfNull(signingKey);
+
+        builder.Services.AddFastEndpoints();
+        builder.Services.AddJWTBearerAuth(signingKey);
+    }
+
+    public static void ConfigureDatabase(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddDbContextPool<AppDbContext>(
+            options => options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
+    }
+
+    public static void MapConfiguration(this WebApplicationBuilder builder)
+    {
+        builder.Services.Configure<SecurityConfiguration>(builder.Configuration.GetRequiredSection("Security"));
+        builder.Services.AddSingleton(resolver =>
+            resolver.GetRequiredService<IOptions<SecurityConfiguration>>().Value);
+    }
+
+    public static void RegisterDependencies(this WebApplicationBuilder builder)
+    {
+        builder.Services.AddSingleton<AuthService>();
+    }
+
+    public static void ConfigureSwaggerDocuments(this WebApplicationBuilder builder)
+    {
+        void Tags(Dictionary<string, string> t)
+        {
+            t["Roles"] = "API of users' roles (student, tutor, administrator)";
+            t["Courses"] = "API of courses (groups of materials and assignments)";
+            t["Groups"] = "API of groups of students";
+            t["Users"] = "API of users";
+            t["Auth"] = "API for signing in/up users";
+        }
+
+        void DocumentSettings(AspNetCoreOpenApiDocumentGeneratorSettings s)
+        {
+            s.Title = "UNI API";
+            s.Description = "API of the learning management system \"UNI\"";
+            s.DocumentName = "v1";
+            s.Version = "v1";
+        }
+
+        builder.Services.SwaggerDocument(o =>
+        {
+            o.AutoTagPathSegmentIndex = 0;
+            o.MaxEndpointVersion = 1;
+            o.TagDescriptions = Tags;
+            o.DocumentSettings = DocumentSettings;
+        }); 
+    }
+}
--- Backend/Program.cs
+++ Backend/Program.cs
@@ -1,11 +1,6 @@
-using Backend.Configuration;
+using Backend.Extensions;
 using FastEndpoints;
 using FastEndpoints.Swagger;
-using Backend.Data;
-using Backend.Modules.Auth.Services;
-using FastEndpoints.Security;
-using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Options;
 using Serilog;
 using Serilog.Events;
 
@@ -19,67 +14,20 @@ Log.Logger = new LoggerConfiguration()
     .CreateLogger();
 
 builder.Host.UseSerilog();
-builder.Services.AddFastEndpoints();
-builder.Services.AddJWTBearerAuth(builder
-        .Configuration
-        .GetRequiredSection("Security")
-        .GetRequiredSection("SigningKey").Value!
-);
-builder.Services.AddDbContextPool<AppDbContext>(
-    options => options.UseNpgsql(builder.Configuration.GetConnectionString("Default")));
 
-builder.Services.Configure<SecurityConfiguration>(builder.Configuration.GetRequiredSection("Security"));
-builder.Services.AddSingleton(resolver =>
-    resolver.GetRequiredService<IOptions<SecurityConfiguration>>().Value);
-
-builder.Services.AddSingleton<AuthService>();
-
-builder.Services.SwaggerDocument(o =>
-{
-    o.AutoTagPathSegmentIndex = 0;
-    o.MaxEndpointVersion = 1;
-    o.TagDescriptions = t =>
-    {
-        t["Roles"] = "API of users' roles (student, tutor, administrator)";
-        t["Courses"] = "API of courses (groups of materials and assignments)";
-        t["Groups"] = "API of groups of students";
-        t["Users"] = "API of users";
-        t["Auth"] = "API for signing in/up users";
-    };
-    o.DocumentSettings = s =>
-    {
-        s.Title = "UNI API";
-        s.Description = "API of the learning management system \"UNI\"";
-        s.DocumentName = "v1";
-        s.Version = "v1";
-    };
-});
+builder.ConfigureFastEndpoints();
+builder.ConfigureDatabase();
+builder.MapConfiguration();
+builder.RegisterDependencies();
+builder.ConfigureSwaggerDocuments();
 
 var app = builder.Build();
+
 app.UseSerilogRequestLogging();
-app.UseAuthentication();
-app.UseAuthorization();
+app.ConfigureAuthorization();
 app.UseDefaultExceptionHandler();
-app.UseFastEndpoints(c =>
-{
-    c.Versioning.Prefix = "v";
-    c.Versioning.PrependToRoute = true;
-});
+app.ConfigureFastEndpoints();
 app.UseSwaggerGen();
-
-using var scope = app.Services.CreateScope();
-var services = scope.ServiceProvider;
-
-var context = services.GetRequiredService<AppDbContext>();
-
-Log.Logger.Information("Checking if has pending migrations...");
-
-if (context.Database.GetPendingMigrations().Any())
-{
-    Log.Logger.Information(
-        "Found pending migrations: {GetPendingMigrations}, migrating...",
-        context.Database.GetPendingMigrations());
-    context.Database.Migrate();
-}
+app.ApplyMigrations();
 
 app.Run();
