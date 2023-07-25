8593b46d2e07f8264de514c0b056cae48b2bd6dd
Daniil <dadyarri@gmail.com>
2023-07-25 21:13:41 +0300

    change id of static file table from generated guid to file id

--- Backend/Modules/Static/Contracts/StaticFile.cs
+++ Backend/Modules/Static/Contracts/StaticFile.cs
@@ -2,8 +2,9 @@
 
 namespace Backend.Modules.Static.Contracts;
 
-public class StaticFile: BaseModel
+public class StaticFile
 {
+    public required string Id { get; set; }
     public required string Checksum { get; set; }
     public required string VisibleName { get; set; }
     public required string FileName { get; set; }
