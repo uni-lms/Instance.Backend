e42be5449af0aae743ab86b8d5cd908c2962c96b
Daniil <dadyarri@gmail.com>
2023-07-25 14:37:59 +0300

    add method to get courses student enrolled in

---
+++ Backend/Modules/Courses/Contract/EnrolledCoursesFilterRequest.cs
@@ -0,0 +1,10 @@
+﻿using FastEndpoints;
+
+namespace Backend.Modules.Courses.Contract;
+
+public class EnrolledCoursesFilterRequest
+{
+    [QueryParam]
+    [BindFrom("filter")]
+    public required string Filter { get; set; }
+}
---
+++ Backend/Modules/Courses/Endpoints/GetEnrolledCourses.cs
@@ -0,0 +1,89 @@
+﻿using System.Net.Mime;
+using Backend.Data;
+using Backend.Modules.Courses.Contract;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Modules.Courses.Endpoints;
+
+public class GetEnrolledCourses : Endpoint<EnrolledCoursesFilterRequest, List<CourseDto>, CoursesMapper>
+{
+    private static readonly string[] AllowedFilters = new[] { "archived", "current", "upcoming" };
+    private readonly AppDbContext _db;
+
+    public GetEnrolledCourses(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public override void Configure()
+    {
+        Get("/courses/enrolled");
+        Description(b => b
+            .Produces<List<CourseDto>>(200, MediaTypeNames.Application.Json)
+            .ProducesProblemFE<InternalErrorResponse>(500));
+        Options(x => x.WithTags("Courses"));
+        Summary(s =>
+        {
+            s.Summary = "Get all courses, in which current user is enrolled (with filter by semester)";
+            s.Description = "Allowed roles: students";
+            s.RequestParam(
+                r => r.Filter,
+                """Filtering rule for courses.<br>Allowed values: <code>archived</code>, <code>current</code>, <code>upcoming</code>""");
+        });
+        Version(1);
+        Roles("Student");
+    }
+
+    public override async Task HandleAsync(EnrolledCoursesFilterRequest req, CancellationToken ct)
+    {
+        if (!AllowedFilters.Contains(req.Filter))
+        {
+            ThrowError(_ => "filter", $"Specified filter {req.Filter} is not allowed");
+        }
+
+        if (User.Identity is null)
+        {
+            ThrowError(_ => User, "Not authorized", 401);
+        }
+
+        var user = await _db.Users
+            .Where(e => e.Email == User.Identity.Name)
+            .FirstOrDefaultAsync(ct);
+
+        if (user is null)
+        {
+            ThrowError(_ => User.Identity.Name, "User not found", 404);
+        }
+
+        var groupOfUser = await _db.Groups.Where(e => e.Students.Contains(user)).FirstOrDefaultAsync(ct);
+
+        if (groupOfUser is null)
+        {
+            ThrowError(_ => "Group", "User doesn't exist in any of groups");
+        }
+
+        ;
+
+        var filteredCourses = req.Filter switch
+        {
+            "archived" => _db.Courses.Where(e =>
+                    e.AssignedGroups.Contains(groupOfUser) && e.Semester < groupOfUser.CurrentSemester)
+                .Include(e => e.Owners)
+                .ThenInclude(e => e.Role)
+                .Select(e => Map.FromEntity(e)),
+            "current" => _db.Courses.Where(e =>
+                    e.AssignedGroups.Contains(groupOfUser) && e.Semester == groupOfUser.CurrentSemester)
+                .Include(e => e.Owners)
+                .ThenInclude(e => e.Role)
+                .Select(e => Map.FromEntity(e)),
+            "upcoming" => _db.Courses.Where(e =>
+                    e.AssignedGroups.Contains(groupOfUser) && e.Semester > groupOfUser.CurrentSemester)
+                .Include(e => e.Owners)
+                .ThenInclude(e => e.Role)
+                .Select(e => Map.FromEntity(e)),
+        };
+
+        await SendAsync(await filteredCourses.ToListAsync(ct), 200, ct);
+    }
+}
