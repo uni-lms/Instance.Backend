1d9a9b0be35bcdb0cb5f95d35ca9dff791d98081
Daniil <dadyarri@gmail.com>
2023-07-25 17:41:05 +0300

    make patronymic not required

--- Backend/Modules/Auth/Contracts/SignupRequest.cs
+++ Backend/Modules/Auth/Contracts/SignupRequest.cs
@@ -4,7 +4,7 @@ public class SignupRequest
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
-    public required string Patronymic { get; set; }
+    public required string? Patronymic { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public required string Email { get; set; }
     public Guid Role { get; set; }
--- Backend/Modules/Users/Contract/User.cs
+++ Backend/Modules/Users/Contract/User.cs
@@ -7,7 +7,7 @@ public class User : BaseModel
 {
     public required string FirstName { get; set; }
     public required string LastName { get; set; }
-    public required string Patronymic { get; set; }
+    public string? Patronymic { get; set; }
     public DateOnly DateOfBirth { get; set; }
     public required string Email { get; set; }
     public required byte[] PasswordHash { get; set; }
