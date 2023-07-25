2204512277d8de72d28ddffe5825bb25e1e7ff02
Daniil <dadyarri@gmail.com>
2023-07-25 21:15:02 +0300

    add deleting file

---
+++ Backend/Modules/Static/Contracts/DeleteFileRequest.cs
@@ -0,0 +1,8 @@
+﻿using FastEndpoints;
+
+namespace Backend.Modules.Static.Contracts;
+
+public class DeleteFileRequest
+{
+    public string FileId { get; set; }
+}
---
+++ Backend/Modules/Static/Endpoints/DeleteFile.cs
@@ -0,0 +1,38 @@
+﻿using Backend.Data;
+using Backend.Modules.Static.Contracts;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Modules.Static.Endpoints;
+
+public class DeleteFile : Endpoint<DeleteFileRequest>
+{
+    private readonly AppDbContext _db;
+
+    public DeleteFile(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public override void Configure()
+    {
+        Delete("/static/{FileId}");
+        Options(x => x.WithTags("Static"));
+    }
+
+    public override async Task HandleAsync(DeleteFileRequest req, CancellationToken ct)
+    {
+        var file = await _db.StaticFiles.FindAsync(new object?[] { req.FileId }, cancellationToken: ct);
+
+        if (file is null)
+        {
+            ThrowError("File was not found", 404);
+        }
+        
+        File.Delete(file.FilePath);
+        _db.StaticFiles.Remove(file);
+        await _db.SaveChangesAsync(ct);
+
+        await SendNoContentAsync(ct);
+    }
+}
