373cc57d70acbbff8e3dd273f9a8f826a3e552d4
Daniil <dadyarri@gmail.com>
2023-07-26 14:56:22 +0300

    add changing password

---
+++ Backend/Modules/Auth/Contracts/ChangePasswordRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Modules.Auth.Contracts;
+
+public class ChangePasswordRequest
+{
+    public required string OldPassword { get; set; }
+    public required string NewPassword { get; set; }
+}
---
+++ Backend/Modules/Auth/Endpoints/ChangePassword.cs
@@ -0,0 +1,57 @@
+﻿using Backend.Data;
+using Backend.Modules.Auth.Contracts;
+using Backend.Modules.Auth.Services;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+
+namespace Backend.Modules.Auth.Endpoints;
+
+public class ChangePassword : Endpoint<ChangePasswordRequest, EmptyResponse>
+{
+    private readonly AppDbContext _db;
+    private readonly AuthService _authService;
+
+    public ChangePassword(AppDbContext db, AuthService authService)
+    {
+        _db = db;
+        _authService = authService;
+    }
+
+    public override void Configure()
+    {
+        Post("/auth/change-password");
+        Options(x => x.WithTags("Auth"));
+        Version(1);
+    }
+
+    public override async Task HandleAsync(ChangePasswordRequest req, CancellationToken ct)
+    {
+        var user = await _db.Users.Where(e => e.Email == User.Identity.Name).FirstOrDefaultAsync(ct);
+
+        if (user is null)
+        {
+            ThrowError(
+                e => User.Identity.Name,
+                $"User with email {User.Identity.Name} is not found",
+                404
+            );
+        }
+
+        if (!_authService.ValidateCredentials(user, new LoginRequest
+            {
+                Email = "",
+                Password = req.OldPassword
+            }))
+        {
+            ThrowError("Wrong password", 403);
+        }
+
+        _authService.CreatePasswordHash(req.NewPassword, out var passwordSalt, out var passwordHash);
+
+        user.PasswordHash = passwordHash;
+        user.PasswordSalt = passwordSalt;
+        await _db.SaveChangesAsync(ct);
+
+        await SendAsync(new EmptyResponse(), cancellation: ct);
+    }
+}
