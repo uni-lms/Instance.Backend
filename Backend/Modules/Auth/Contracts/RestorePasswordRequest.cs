b9710bc7284690c5ff6441231b43d4d60de06664
Daniil <dadyarri@gmail.com>
2023-07-26 14:29:35 +0300

    add restoring passwords

---
+++ Backend/Modules/Auth/Contracts/RestorePasswordRequest.cs
@@ -0,0 +1,7 @@
+﻿namespace Backend.Modules.Auth.Contracts;
+
+public class RestorePasswordRequest
+{
+    public required string Email { get; set; }
+    public required string NewPassword { get; set; }
+}
