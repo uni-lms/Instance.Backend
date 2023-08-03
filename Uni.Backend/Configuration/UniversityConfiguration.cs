0cf8b9df349e2225c9b29c82d6f2ca6c86c10354
Daniil <dadyarri@gmail.com>
2023-08-03 20:59:33 +0300

    add university's email address to config

--- Uni.Backend/Configuration/UniversityConfiguration.cs
+++ Uni.Backend/Configuration/UniversityConfiguration.cs
@@ -4,4 +4,5 @@ public class UniversityConfiguration
 {
     public required string Name { get; set; }
     public required string Domain { get; set; }
+    public required string NoReplyEmail { get; set; }
 }
