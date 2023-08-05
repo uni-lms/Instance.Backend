2981e35fb0bdf4fffff388a0a18b7c8a5b7860d2
Daniil <dadyarri@gmail.com>
2023-08-05 16:17:52 +0300

    add editing courses

---
+++ Uni.Backend/Modules/Courses/Contracts/UpdateCourseRequest.cs
@@ -0,0 +1,17 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Uni.Backend.Modules.Courses.Contracts;
+
+public class UpdateCourseRequest
+{
+    [FromRoute]
+    [BindFrom("id")]
+    public Guid Id { get; set; }
+    public required string Name { get; set; }
+    public required string Abbreviation { get; set; }
+    public int Semester { get; set; }
+    public required List<Guid> AssignedGroups { get; set; }
+    public required List<Guid> Owners { get; set; }
+    public required List<Guid> Blocks { get; set; }
+}
