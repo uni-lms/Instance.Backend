30e43c9cfec8ea4c474afc4279dcab05396032bb
Daniil <dadyarri@gmail.com>
2023-08-06 11:57:26 +0300

    add updating file content

---
+++ Uni.Backend/Modules/CourseContents/File/Contracts/UpdateFileContentRequest.cs
@@ -0,0 +1,14 @@
+﻿using FastEndpoints;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Uni.Backend.Modules.CourseContents.File.Contracts;
+
+public class UpdateFileContentRequest
+{
+    [FromRoute]
+    [BindFrom("id")]
+    public Guid Id { get; set; }
+
+    public required string VisibleName { get; set; }
+    public Guid Block { get; set; }
+}
