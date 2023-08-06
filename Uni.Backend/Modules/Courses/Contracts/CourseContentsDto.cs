76300dee8e1a1614fb322d67c9dac3ce43ce1124
Daniil <dadyarri@gmail.com>
2023-08-06 14:42:07 +0300

    add fetching basic information about course

---
+++ Uni.Backend/Modules/Courses/Contracts/CourseContentsDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Uni.Backend.Modules.Courses.Contracts;
+
+public class CourseContentsDto
+{
+    public required string Name { get; set; }
+    public int Semester { get; set; }
+    public required List<string> Owners { get; set; }
+    
+}
---
+++ Uni.Backend/Modules/Courses/Endpoints/GetCourseContents.cs
@@ -0,0 +1,71 @@
+﻿using System.Net.Mime;
+using FastEndpoints;
+using Microsoft.EntityFrameworkCore;
+using Uni.Backend.Data;
+using Uni.Backend.Modules.Common.Contracts;
+using Uni.Backend.Modules.Courses.Contracts;
+
+namespace Uni.Backend.Modules.Courses.Endpoints;
+
+public class GetCourseContents : Endpoint<SearchEntityRequest, CourseContentsDto>
+{
+    private readonly AppDbContext _db;
+
+    public GetCourseContents(AppDbContext db)
+    {
+        _db = db;
+    }
+
+    public override void Configure()
+    {
+        Version(1);
+        Get("/courses/{id}/contents");
+        Options(x => x.WithTags("Courses"));
+        Description(b => b
+            .ClearDefaultProduces()
+            .Produces<CourseContentsDto>(200, MediaTypeNames.Application.Json)
+            .ProducesProblemFE(401)
+            .ProducesProblemFE(404)
+            .ProducesProblemFE(500));
+        Summary(x =>
+        {
+            x.Summary = "Gets content of the course";
+            x.Description = """
+                               <b>Allowed scopes:</b> Any authorized user
+                            """;
+            x.Responses[201] = "Course contents fetched successfully";
+            x.Responses[401] = "Not authorized";
+            x.Responses[404] = "Course was not found";
+            x.Responses[500] = "Some other error occured";
+        });
+    }
+
+    public override async Task HandleAsync(SearchEntityRequest req, CancellationToken ct)
+    {
+        var course = await _db.Courses
+            .AsNoTracking()
+            .Where(e => e.Id == req.Id)
+            .Include(e => e.Owners)
+            .FirstOrDefaultAsync(ct);
+
+        if (course is null)
+        {
+            ThrowError(e => e.Id, "Course was not found", 404);
+        }
+
+        var owners = course.Owners.Select(e =>
+        {
+            var patronymicInitials = e.Patronymic is not null ? $" {e.Patronymic[0]}." : "";
+            return $"{e.LastName} {e.FirstName[0]}.{patronymicInitials}";
+        }).ToList();
+
+        var dto = new CourseContentsDto
+        {
+            Name = course.Name,
+            Semester = course.Semester,
+            Owners = owners
+        };
+
+        await SendAsync(dto, cancellation: ct);
+    }
+}
