c0967a9bec4f5bb90d1343536043005eca087965
Daniil <dadyarri@gmail.com>
2023-08-19 00:11:21 +0300

    add models to create quiz and response to that action

---
+++ Uni.Backend/Modules/CourseContents/Quiz/Contracts/CreateQuizRequest.cs
@@ -0,0 +1,15 @@
+﻿using JetBrains.Annotations;
+
+
+namespace Uni.Backend.Modules.CourseContents.Quiz.Contracts;
+
+public class CreateQuizRequest {
+  public Guid CourseId { get; [UsedImplicitly] set; }
+  public Guid BlockId { get; [UsedImplicitly] set; }
+  public required string Title { get; [UsedImplicitly] set; }
+  public string? Description { get; [UsedImplicitly] set; }
+  public TimeSpan TimeLimit { get; [UsedImplicitly] set; }
+  public bool IsQuestionsShuffled { get; [UsedImplicitly] set; }
+  public DateTime AvailableUntil { get; [UsedImplicitly] set; }
+  public required List<Guid> Questions { get; [UsedImplicitly] set; }
+}
---
+++ Uni.Backend/Modules/CourseContents/Quiz/Contracts/QuizDto.cs
@@ -0,0 +1,7 @@
+﻿namespace Uni.Backend.Modules.CourseContents.Quiz.Contracts; 
+
+public class QuizDto {
+  public required string Title { get; set; }
+  public string? Description { get; set; }
+  public DateTime AvailableUntil { get; set; }
+}
