237061529e83282086b02140b2b14b893357a492
Daniil <dadyarri@gmail.com>
2023-08-19 10:20:19 +0300

    add updating course

---
+++ Uni.Backend/Modules/CourseContents/Quiz/Contracts/UpdateQuizRequest.cs
@@ -0,0 +1,12 @@
+﻿using FastEndpoints;
+
+using Microsoft.AspNetCore.Mvc;
+
+
+namespace Uni.Backend.Modules.CourseContents.Quiz.Contracts;
+
+public class UpdateQuizRequest: CreateQuizRequest {
+  [FromRoute]
+  [BindFrom("id")]
+  public Guid Id { get; set; }
+}
