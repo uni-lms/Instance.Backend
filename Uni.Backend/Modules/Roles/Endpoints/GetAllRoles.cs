113b8e0f9a4c3f1ffeac60b1a94264e6a38ffb0f
Daniil <dadyarri@gmail.com>
2023-08-26 23:08:04 +0300

    fix status code

--- Uni.Backend/Modules/Roles/Endpoints/GetAllRoles.cs
+++ Uni.Backend/Modules/Roles/Endpoints/GetAllRoles.cs
@@ -29,7 +29,7 @@ public class GetAllRoles : EndpointWithoutRequest<List<Role>> {
       x.Summary = "Gets all available user roles";
       x.Description = "<b>Allowed scopes:</b> Any authorized user";
       x.Responses[200] = "List of roles fetched successfully";
-      x.Responses[200] = "Unauthorized";
+      x.Responses[401] = "Unauthorized";
       x.Responses[500] = "Some other error occured";
     });
   }
