fdfb5b45044b7ba99543876d83278cdff76ef1d5
Daniil <dadyarri@gmail.com>
2023-08-28 12:07:38 +0300

    little refactor

--- Uni.Backend/Modules/Assignments/Contracts/Assignment.cs
+++ Uni.Backend/Modules/Assignments/Contracts/Assignment.cs
@@ -1,16 +1,11 @@
-﻿using Uni.Backend.Data;
-using Uni.Backend.Modules.CourseBlocks.Contracts;
-using Uni.Backend.Modules.Courses.Contracts;
+﻿using Uni.Backend.Modules.CourseContents.Abstractions;
 
 
 namespace Uni.Backend.Modules.Assignments.Contracts;
 
-public class Assignment: BaseModel {
-  public required Course Course { get; set; }
-  public required CourseBlock Block { get; set; }
+public class Assignment: BaseCourseContent {
   public required string Title { get; set; }
   public string? Description { get; set; }
   public DateTime AvailableUntil { get; set; }
-  public bool IsVisibleToStudents { get; set; }
   public int MaximumPoints { get; set; }
 }
