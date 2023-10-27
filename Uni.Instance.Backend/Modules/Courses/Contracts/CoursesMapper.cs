53894c998a1b2fe3381ce92f74cff641351b3ff3
Daniil <dadyarri@gmail.com>
2023-10-27 13:31:15 +0300

    add second version of get enrolled courses

--- Uni.Instance.Backend/Modules/Courses/Contracts/CoursesMapper.cs
+++ Uni.Instance.Backend/Modules/Courses/Contracts/CoursesMapper.cs
@@ -2,10 +2,33 @@
 
 using Riok.Mapperly.Abstractions;
 
+using Uni.Backend.Modules.Courses.Contracts;
+using Uni.Backend.Modules.Users.Contracts;
 
-namespace Uni.Backend.Modules.Courses.Contracts;
+
+namespace Uni.Instance.Backend.Modules.Courses.Contracts;
 
 [Mapper]
 public partial class CoursesMapper : ResponseMapper<CourseDto, Course> {
   public partial CourseDto FromEntity(Course r);
+
+  public CourseDtoV2 FromEntityToV2(Course r) {
+    return new CourseDtoV2 {
+      Abbreviation = r.Abbreviation,
+      Name = r.Name,
+      Progress = 90,
+      Id = r.Id,
+      Semester = r.Semester,
+      Tutors = r.Owners.Select(UserDtoToString).ToList(),
+    };
+  }
+
+  private string UserDtoToString(User u) {
+    var patronymic = "";
+    if (u.Patronymic is not null) {
+      patronymic = $" {u.Patronymic[0]}.";
+    }
+
+    return $"{u.LastName} {u.FirstName[0]}.{patronymic}";
+  }
 }
