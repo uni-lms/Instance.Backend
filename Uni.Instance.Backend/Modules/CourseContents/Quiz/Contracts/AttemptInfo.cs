58b5856ffe2cd45a0f1b2633113cb4b2925cda24
Daniil <dadyarri@gmail.com>
2023-11-07 12:32:51 +0300

    add method to fetch quiz info

---
+++ Uni.Instance.Backend/Modules/CourseContents/Quiz/Contracts/AttemptInfo.cs
@@ -0,0 +1,7 @@
+﻿namespace Uni.Instance.Backend.Modules.CourseContents.Quiz.Contracts; 
+
+public class AttemptInfo {
+  public DateTime StartedAt { get; set; }
+  public DateTime? FinishedAt { get; set; }
+  public int AccruedPoints { get; set; }
+}
