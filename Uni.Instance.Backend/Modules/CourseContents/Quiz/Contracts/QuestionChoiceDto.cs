73a525509d6b559786121d90cd129f049f48b5a3
Daniil <dadyarri@gmail.com>
2023-11-09 00:38:00 +0300

    fix logic error

--- Uni.Instance.Backend/Modules/CourseContents/Quiz/Contracts/QuestionChoiceDto.cs
+++ Uni.Instance.Backend/Modules/CourseContents/Quiz/Contracts/QuestionChoiceDto.cs
@@ -4,5 +4,6 @@
 namespace Uni.Instance.Backend.Modules.CourseContents.Quiz.Contracts;
 
 public class QuestionChoiceDto : BaseModel {
+  public required Guid QuestionId { get; set; }
   public required string Title { get; set; }
 }
