e313c5a5bf6f9932844a98abb2dec34ffd3ca4ec
Daniil <dadyarri@gmail.com>
2023-11-09 01:00:28 +0300

    fixes

---
+++ Uni.Instance.Backend/Modules/CourseContents/Quiz/Contracts/QuestionChoiceRequest.cs
@@ -0,0 +1,8 @@
+﻿using Uni.Backend.Data;
+
+
+namespace Uni.Instance.Backend.Modules.CourseContents.Quiz.Contracts; 
+
+public class QuestionChoiceRequest: BaseModel {
+  
+}
--- Uni.Instance.Backend/Modules/CourseContents/Quiz/Contracts/SaveAnswerForQuestionRequest.cs
+++ Uni.Instance.Backend/Modules/CourseContents/Quiz/Contracts/SaveAnswerForQuestionRequest.cs
@@ -1,7 +1,5 @@
 ﻿using Microsoft.AspNetCore.Mvc;
 
-using Uni.Backend.Data;
-
 
 namespace Uni.Instance.Backend.Modules.CourseContents.Quiz.Contracts;
 
@@ -10,5 +8,5 @@ public class SaveAnswerForQuestionRequest {
   public Guid AttemptId { get; set; }
 
   public Guid QuestionId { get; set; }
-  public required List<BaseModel> Choices { get; set; }
+  public required List<QuestionChoiceRequest> Choices { get; set; }
 }
