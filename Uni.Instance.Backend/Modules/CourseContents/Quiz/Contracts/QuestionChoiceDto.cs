5067c578ef1ba5e9bc335137812ba7bf1adcdd53
Daniil <dadyarri@gmail.com>
2023-11-09 00:58:00 +0300

    more fixes

--- Uni.Instance.Backend/Modules/CourseContents/Quiz/Contracts/QuestionChoiceDto.cs
+++ Uni.Instance.Backend/Modules/CourseContents/Quiz/Contracts/QuestionChoiceDto.cs
@@ -4,6 +4,5 @@
 namespace Uni.Instance.Backend.Modules.CourseContents.Quiz.Contracts;
 
 public class QuestionChoiceDto : BaseModel {
-  public Guid? QuestionId { get; set; }
   public required string Title { get; set; }
 }
