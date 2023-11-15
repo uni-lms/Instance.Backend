4b311ee08f92754539b5e7a5f25fc98d30198c1e
Daniil <dadyarri@gmail.com>
2023-11-15 18:27:49 +0300

    add endpoint to get assignment's info

--- Uni.Instance.Backend/Modules/Assignments/Contracts/AssignmentDto.cs
+++ Uni.Instance.Backend/Modules/Assignments/Contracts/AssignmentDto.cs
@@ -5,10 +5,7 @@ namespace Uni.Backend.Modules.Assignments.Contracts;
 
 public class AssignmentDto {
   public Guid Id { [UsedImplicitly] get; set; }
-  public required string BlockName { [UsedImplicitly] get; set; }
   public required string Title { [UsedImplicitly] get; set; }
   public string? Description { [UsedImplicitly] get; set; }
   public DateTime AvailableUntil { [UsedImplicitly] get; set; }
-  public bool IsVisibleToStudents { [UsedImplicitly] get; set; }
-  public int MaximumPoints { [UsedImplicitly] get; set; }
 }
---
+++ Uni.Instance.Backend/Modules/Assignments/Endpoints/GetAssignmentInfo.cs
@@ -0,0 +1,60 @@
+﻿using System.Net.Mime;
+
+using FastEndpoints;
+
+using Microsoft.EntityFrameworkCore;
+
+using Uni.Backend.Data;
+using Uni.Backend.Modules.Assignments.Contracts;
+using Uni.Backend.Modules.Common.Contracts;
+
+
+namespace Uni.Instance.Backend.Modules.Assignments.Endpoints; 
+
+public class GetAssignmentInfo: Endpoint<SearchEntityRequest, AssignmentDto> {
+  private readonly AppDbContext _db;
+
+  public GetAssignmentInfo(AppDbContext db) {
+    _db = db;
+  }
+
+  public override void Configure() {
+    Version(1);
+    Post("/assignments/{id}");
+    Options(x => x.WithTags("Assignments"));
+    Description(b => b
+      .Produces<Assignment>(200, MediaTypeNames.Application.Json)
+      .ProducesProblemFE(401)
+      .ProducesProblemFE(404)
+      .ProducesProblemFE(500));
+    Summary(x => {
+      x.Summary = "Gets info of assignment";
+      x.Description = "<b>Allowed scopes:</b> Any authorized user";
+      x.Responses[201] = "Information was fetched successfully";
+      x.Responses[401] = "Unauthorized";
+      x.Responses[403] = "Forbidden";
+      x.Responses[404] = "Some related entity was not found";
+      x.Responses[500] = "Some other error occured";
+    });
+  }
+
+  public override async Task HandleAsync(SearchEntityRequest req, CancellationToken ct) {
+    var assignment = await _db.Assignments
+      .AsNoTracking()
+      .Where(e => e.Id == req.Id)
+      .FirstOrDefaultAsync(ct);
+
+    if (assignment is null) {
+      ThrowError(e => e.Id, "Assignment was not found");
+    }
+
+    var dto = new AssignmentDto {
+      Title = assignment.Title,
+      Description = assignment.Description,
+      AvailableUntil = assignment.AvailableUntil,
+      Id = assignment.Id,
+    };
+
+    await SendAsync(dto, cancellation: ct);
+  }
+}
