b9d8a884adbfc5a2e3e15c7d9a781eab01bd1e55
Daniil <dadyarri@gmail.com>
2023-11-20 21:43:44 +0300

    change json conversion method for status enum

--- Uni.Instance.Backend/Modules/Assignments/Contracts/AssignmentDto.cs
+++ Uni.Instance.Backend/Modules/Assignments/Contracts/AssignmentDto.cs
@@ -1,4 +1,6 @@
-﻿using JetBrains.Annotations;
+﻿using System.Text.Json.Serialization;
+
+using JetBrains.Annotations;
 
 using Uni.Backend.Modules.SolutionChecks.Contracts;
 
@@ -10,7 +12,10 @@ public class AssignmentDto {
   public required string Title { [UsedImplicitly] get; set; }
   public string? Description { [UsedImplicitly] get; set; }
   public DateTime AvailableUntil { [UsedImplicitly] get; set; }
+
+  [JsonConverter(typeof(JsonStringEnumConverter))]
   public SolutionCheckStatus Status { get; set; }
+
   public required int MaximumPoints { [UsedImplicitly] get; set; }
   public required int Rating { [UsedImplicitly] get; set; }
 }
