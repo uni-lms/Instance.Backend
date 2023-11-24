936fe833c5b05d39694bc3341985e2d9a7fc4822
Daniil <dadyarri@gmail.com>
2023-11-24 16:08:32 +0300

    make list of users return less data

--- Uni.Instance.Backend/Modules/Users/Contracts/UserDto.cs
+++ Uni.Instance.Backend/Modules/Users/Contracts/UserDto.cs
@@ -7,8 +7,5 @@ public class UserDto {
   public required string FirstName { [UsedImplicitly] get; set; }
   public required string LastName { [UsedImplicitly] get; set; }
   public string? Patronymic { [UsedImplicitly] get; set; }
-  public DateOnly? DateOfBirth { [UsedImplicitly] get; set; }
-  public string? Email { [UsedImplicitly] get; set; }
   public string? RoleName { get; set; }
-  public string? GenderName { get; set; }
 }
--- Uni.Instance.Backend/Modules/Users/Contracts/UserMapper.cs
+++ Uni.Instance.Backend/Modules/Users/Contracts/UserMapper.cs
@@ -8,6 +8,5 @@ namespace Uni.Backend.Modules.Users.Contracts;
 [Mapper]
 public partial class UserMapper : ResponseMapper<UserDto, User> {
   [MapProperty(nameof(User.Role), nameof(UserDto.RoleName))]
-  [MapProperty(nameof(User.Gender), nameof(UserDto.GenderName))]
   public partial UserDto FromEntity(User e);
 }
