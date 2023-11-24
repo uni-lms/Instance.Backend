1de70a97fe749c9937801c272ee99a94e9f74336
Daniil <dadyarri@gmail.com>
2023-11-24 16:10:38 +0300

    add id

--- Uni.Instance.Backend/Modules/Users/Contracts/UserDto.cs
+++ Uni.Instance.Backend/Modules/Users/Contracts/UserDto.cs
@@ -4,6 +4,7 @@
 namespace Uni.Backend.Modules.Users.Contracts;
 
 public class UserDto {
+  public required Guid Id { get; set; }
   public required string FirstName { [UsedImplicitly] get; set; }
   public required string LastName { [UsedImplicitly] get; set; }
   public string? Patronymic { [UsedImplicitly] get; set; }
