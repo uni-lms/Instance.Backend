65f69b5d9fb4ecb849cef661b09db6b6ed074ffb
Daniil <dadyarri@gmail.com>
2023-12-07 17:01:12 +0300

    update response

--- Uni.Instance.Backend/Modules/CourseContents/Abstractions/BaseCourseContent.cs
+++ Uni.Instance.Backend/Modules/CourseContents/Abstractions/BaseCourseContent.cs
@@ -1,4 +1,6 @@
-﻿using Uni.Backend.Data;
+﻿using System.Text.Json.Serialization;
+
+using Uni.Backend.Data;
 using Uni.Backend.Modules.CourseBlocks.Contracts;
 using Uni.Backend.Modules.Courses.Contracts;
 
@@ -6,6 +8,7 @@ using Uni.Backend.Modules.Courses.Contracts;
 namespace Uni.Backend.Modules.CourseContents.Abstractions;
 
 public abstract class BaseCourseContent : BaseModel {
+  [JsonIgnore]
   public required Course Course { get; init; }
 
   public required CourseBlock Block { get; set; }
