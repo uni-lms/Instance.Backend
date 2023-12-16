e1319e1526035445deaa45e611970bf18ec73465
Daniil <dadyarri@gmail.com>
2023-12-16 19:51:07 +0300

    add list of default roles; add some configs

--- Uni.Instance.Backend/Data/AppDbContext.cs
+++ Uni.Instance.Backend/Data/AppDbContext.cs
@@ -11,5 +11,11 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
 
   protected override void OnModelCreating(ModelBuilder modelBuilder) {
     modelBuilder.Entity<User>().HasIndex(e => e.Email);
+
+    modelBuilder.Entity<Role>().HasData(new List<Role>() {
+      new() { Id = Guid.Parse("9a6bcced-f1d3-4155-b691-480718ee78e4"), Name = "Admin" },
+      new() { Id = Guid.Parse("c82073b9-126c-4b4e-8edc-bc3d0cea56f1"), Name = "Tutor" },
+      new() { Id = Guid.Parse("c0aa6455-626c-49de-94fc-616b4a6b5605"), Name = "Student" },
+    });
   }
 }
