494ef8f016c9b01b99a9b87c42f5a0182c774bda
Daniil <dadyarri@gmail.com>
2023-12-17 11:17:30 +0300

    rewrite ping endpoint

--- Uni.Instance.Backend/Endpoints/Internal/PingEndpoint.cs
+++ Uni.Instance.Backend/Endpoints/Internal/PingEndpoint.cs
@@ -1,22 +1,29 @@
-﻿using FastEndpoints;
+﻿using Ardalis.Result;
 
-using FluentResults;
+using FastEndpoints;
 
 using JetBrains.Annotations;
+
+using Uni.Instance.Backend.Endpoints.Internal.Data;
+using Uni.Instance.Backend.Endpoints.Internal.Services;
+using Uni.Instance.Backend.Extensions;
 
 
 namespace Uni.Instance.Backend.Endpoints.Internal;
 
 [UsedImplicitly]
-public sealed class PingEndpoint : Endpoint<EmptyRequest, Result<string>> {
+public sealed class PingEndpoint(PingService service) : Endpoint<PingRequest, Result<string>> {
   public override void Configure() {
     Version(2);
     Get("/internal/ping");
     Options(x => x.WithTags("Internal"));
     AllowAnonymous();
+    DontThrowIfValidationFails();
   }
 
-  public override async Task<Result<string>> ExecuteAsync(EmptyRequest req, CancellationToken ct) {
-    return Result.Ok("pong");
+  public override async Task HandleAsync(PingRequest req, CancellationToken ct) {
+    var result = service.Ping(ValidationFailed, ValidationFailures);
+
+    await this.SendResponseAsync(result, cancellation: ct);
   }
 }
