6b0fe02a2ebfa15a29bd18d57e6d28dd2e65445d
Daniil <dadyarri@gmail.com>
2024-01-10 13:53:41 +0300

    add signup endpoint summary

--- Uni.Instance.Backend/Endpoints/Auth/Endpoints/Signup/SignupEndpoint.cs
+++ Uni.Instance.Backend/Endpoints/Auth/Endpoints/Signup/SignupEndpoint.cs
@@ -8,10 +8,10 @@ using Uni.Instance.Backend.Endpoints.Auth.Services;
 using Uni.Instance.Backend.Extensions;
 
 
-namespace Uni.Instance.Backend.Endpoints.Auth;
+namespace Uni.Instance.Backend.Endpoints.Auth.Endpoints.Signup;
 
 [UsedImplicitly]
-public class SignUpEndpoint(AuthService service) : Endpoint<SignUpRequest> {
+public class SignupEndpoint(AuthService service) : Endpoint<SignupRequest> {
   public override void Configure() {
     Post("/auth/sign-up");
     Version(2);
@@ -20,7 +20,7 @@ public class SignUpEndpoint(AuthService service) : Endpoint<SignUpRequest> {
     DontThrowIfValidationFails();
   }
 
-  public override async Task HandleAsync(SignUpRequest req, CancellationToken ct) {
+  public override async Task HandleAsync(SignupRequest req, CancellationToken ct) {
     var result = await service.SignUpAsync(ValidationFailed, ValidationFailures, req, ct);
 
     await this.SendResponseAsync(result, cancellation: ct);
---
+++ Uni.Instance.Backend/Endpoints/Auth/Endpoints/Signup/SignupEndpointSummary.cs
@@ -0,0 +1,29 @@
+﻿using Ardalis.Result;
+
+using FastEndpoints;
+
+using JetBrains.Annotations;
+
+using Uni.Instance.Backend.Endpoints.Auth.Data;
+
+
+namespace Uni.Instance.Backend.Endpoints.Auth.Endpoints.Signup;
+
+[UsedImplicitly]
+public class SignupEndpointSummary : Summary<SignupEndpoint> {
+  public SignupEndpointSummary() {
+    Summary = "Создаёт нового пользователя и возвращает его токен доступа";
+    Description = "<b>Может использоваться:</b> Анонимным пользователем";
+    ExampleRequest = new SignupRequest {
+      Email = "foo@bar.com",
+      Password = "p@ssW0rd",
+      FirstName = "Иван",
+      LastName = "Иванов",
+      RoleId = new Guid("c82073b9-126c-4b4e-8edc-bc3d0cea56f1"),
+    };
+    Response<Result<LoginResponse>>(200, "Успешная регистрация");
+    Response<Result<ErrorResponse>>(400, "Неверный запрос");
+    Response<Result<ErrorResponse>>(404, "Несуществующая роль");
+    Response<Result<ErrorResponse>>(409, "Пользователь с таким Email уже существует");
+  }
+}
