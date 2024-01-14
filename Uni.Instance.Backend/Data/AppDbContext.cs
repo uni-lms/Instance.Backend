2fe15e0fb5e9120d1f921913ed65f67183cd790d
Daniil <dadyarri@gmail.com>
2024-01-14 11:22:08 +0300

    add course section table

--- Uni.Instance.Backend/Data/AppDbContext.cs
+++ Uni.Instance.Backend/Data/AppDbContext.cs
@@ -8,6 +8,7 @@ namespace Uni.Instance.Backend.Data;
 public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(options) {
   public virtual DbSet<User> Users => Set<User>();
   public virtual DbSet<Role> Roles => Set<Role>();
+  public virtual DbSet<CourseSection> CourseSections => Set<CourseSection>();
 
   protected override void OnModelCreating(ModelBuilder modelBuilder) {
     modelBuilder.Entity<User>().HasIndex(e => e.Email);
@@ -16,6 +17,16 @@ public class AppDbContext(DbContextOptions<AppDbContext> options) : DbContext(op
       new() { Id = Guid.Parse("9a6bcced-f1d3-4155-b691-480718ee78e4"), Name = "Admin" },
       new() { Id = Guid.Parse("c82073b9-126c-4b4e-8edc-bc3d0cea56f1"), Name = "Tutor" },
       new() { Id = Guid.Parse("c0aa6455-626c-49de-94fc-616b4a6b5605"), Name = "Student" },
+    });
+
+    modelBuilder.Entity<CourseSection>().HasData(new List<CourseSection> {
+      new() { Id = new Guid("a19c6184-46c3-40fa-bef7-32ab966a4946"), Name = "Лекции" },
+      new() { Id = new Guid("965d07a0-8666-49a9-af0e-797944c65f5f"), Name = "Лабораторные работы" },
+      new() { Id = new Guid("adb191be-1e90-46e2-9886-1bfe911a0d66"), Name = "Самостоятельная работа студентов" },
+      new() { Id = new Guid("d2604c2a-550a-4f7e-8da8-1864e6337377"), Name = "Экзамен" },
+      new() { Id = new Guid("d10a8149-afb3-416b-8bfe-3dbbedbec517"), Name = "Курсовой проект" },
+      new() { Id = new Guid("fa013e01-8a58-4ba5-b426-72b790c3ba4f"), Name = "Курсовая работа" },
+      new() { Id = new Guid("33fe1815-0e3b-4991-8add-c163a2286711"), Name = "Зачёт" },
     });
   }
 }
---
+++ Uni.Instance.Backend/Data/Migrations/20240114081956_AddCourseSectionsTable.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+#pragma warning disable CA1814 // Prefer jagged arrays over multidimensional
+
+namespace Uni.Instance.Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddCourseSectionsTable : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "CourseSections",
+                columns: table => new
+                {
+                    Id = table.Column<Guid>(type: "uuid", nullable: false),
+                    Name = table.Column<string>(type: "character varying(40)", maxLength: 40, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_CourseSections", x => x.Id);
+                });
+
+            migrationBuilder.InsertData(
+                table: "CourseSections",
+                columns: new[] { "Id", "Name" },
+                values: new object[,]
+                {
+                    { new Guid("33fe1815-0e3b-4991-8add-c163a2286711"), "Зачёт" },
+                    { new Guid("965d07a0-8666-49a9-af0e-797944c65f5f"), "Лабораторные работы" },
+                    { new Guid("a19c6184-46c3-40fa-bef7-32ab966a4946"), "Лекции" },
+                    { new Guid("adb191be-1e90-46e2-9886-1bfe911a0d66"), "Самостоятельная работа студентов" },
+                    { new Guid("d10a8149-afb3-416b-8bfe-3dbbedbec517"), "Курсовой проект" },
+                    { new Guid("d2604c2a-550a-4f7e-8da8-1864e6337377"), "Экзамен" },
+                    { new Guid("fa013e01-8a58-4ba5-b426-72b790c3ba4f"), "Курсовая работа" }
+                });
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "CourseSections");
+        }
+    }
+}
