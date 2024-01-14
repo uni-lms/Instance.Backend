47edbd18de069a6f7e3e0ddfb62f370096c992d2
Daniil <dadyarri@gmail.com>
2024-01-14 13:06:04 +0300

    add group tag

--- Uni.Instance.Backend/Configuration/Swagger/ApiTags.cs
+++ Uni.Instance.Backend/Configuration/Swagger/ApiTags.cs
@@ -3,6 +3,7 @@
 public static class ApiTags {
   public static readonly ApiTag Internal = new("Internal", "Внутренние служебные методы");
   public static readonly ApiTag Auth = new("Auth", "Методы, связанные с управлением пользовательскими аккаунтами");
+  public static readonly ApiTag Groups = new("Groups", "Методы, связанные с управлением группами");
 
   public static readonly ApiTag CourseSections =
     new("CourseSections", "Методы, связанные с управлением секциями в курсе");
