22a45a155fd60f74382f235898e8d4af84eb2071
Daniil <dadyarri@gmail.com>
2024-01-14 13:05:42 +0300

    add roles constraints for endpoints

--- Uni.Instance.Backend/Configuration/Swagger/CanBeUsedBy.cs
+++ Uni.Instance.Backend/Configuration/Swagger/CanBeUsedBy.cs
@@ -7,4 +7,9 @@ public static class CanBeUsedBy {
   public const string AnyStudent = $"{UsedBy} Любым <i>студентом</i>";
   public const string AnyTutor = $"{UsedBy} Любым <i>преподавателем</i>";
   public const string AnyAdmin = $"{UsedBy} Любым <i>администратором</i>";
+
+  public static readonly string[] OnlyStudent = ["Student"];
+  public static readonly string[] OnlyTutor = ["Tutor"];
+  public static readonly string[] OnlyAdmin = ["Admin"];
+  public static readonly string[] TutorAndAbove = ["Tutor", "Admin"];
 }
