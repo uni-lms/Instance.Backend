0f204f53d70f96b620f56035b2bdbf4753a8c69c
Daniil <dadyarri@gmail.com>
2024-01-17 12:48:21 +0300

    translate api tags to russian

--- Uni.Instance.Backend/Configuration/Swagger/ApiTags.cs
+++ Uni.Instance.Backend/Configuration/Swagger/ApiTags.cs
@@ -1,13 +1,16 @@
 ﻿namespace Uni.Instance.Backend.Configuration.Swagger;
 
 public static class ApiTags {
-  public static readonly ApiTag Internal = new("Internal", "Внутренние служебные методы");
-  public static readonly ApiTag Auth = new("Auth", "Методы, связанные с управлением пользовательскими аккаунтами");
-  public static readonly ApiTag Groups = new("Groups", "Методы, связанные с управлением группами");
-  public static readonly ApiTag Users = new("Users", "Методы, связанные с управлением пользователями");
+  public static readonly ApiTag Internal = new("Служебные", "Внутренние служебные методы");
+
+  public static readonly ApiTag Auth = new("Авторизация",
+    "Методы, связанные с управлением пользовательскими аккаунтами");
+
+  public static readonly ApiTag Groups = new("Группы", "Методы, связанные с управлением группами");
+  public static readonly ApiTag Users = new("Пользователи", "Методы, связанные с управлением пользователями");
 
   public static readonly ApiTag CourseSections =
-    new("CourseSections", "Методы, связанные с управлением секциями в курсе");
+    new("Разделы курсов", "Методы, связанные с управлением секциями в курсе");
 }
 
 public record ApiTag(string Tag, string Description);
