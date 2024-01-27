3fbd0c35d60c77db77dd30bc983f2a7df46fc07f
Daniil <dadyarri@gmail.com>
2024-01-28 00:50:52 +0300

    add endpoint to delete course

---
+++ Uni.Instance.Backend/Api/Course/Endpoints/Delete/DeleteCourseEndpoint.cs
@@ -0,0 +1,25 @@
+﻿using FastEndpoints;
+
+using Uni.Instance.Backend.Api.Course.Data;
+using Uni.Instance.Backend.Api.Course.Services;
+using Uni.Instance.Backend.Configuration.Swagger;
+using Uni.Instance.Backend.Data.Common;
+using Uni.Instance.Backend.Extensions;
+
+
+namespace Uni.Instance.Backend.Api.Course.Endpoints.Delete;
+
+public class DeleteCourseEndpoint(CoursesService service) : Endpoint<SearchByIdModel, BaseCourseDto> {
+  public override void Configure() {
+    Version(2);
+    Delete("/course/{id}");
+    Roles(CanBeUsedBy.TutorAndAbove);
+    Options(x => x.WithTags(ApiTags.Courses.Tag));
+    DontThrowIfValidationFails();
+  }
+
+  public override async Task HandleAsync(SearchByIdModel req, CancellationToken ct) {
+    var result = await service.DeleteCourseAsync(req, ct);
+    await this.SendResponseAsync(result, ct);
+  }
+}
---
+++ Uni.Instance.Backend/Api/Course/Endpoints/Delete/DeleteCourseEndpointSummary.cs
@@ -0,0 +1,20 @@
+﻿using Ardalis.Result;
+
+using FastEndpoints;
+
+using Uni.Instance.Backend.Api.Course.Data;
+using Uni.Instance.Backend.Configuration.Swagger;
+
+
+namespace Uni.Instance.Backend.Api.Course.Endpoints.Delete;
+
+public class DeleteCourseEndpointSummary : Summary<DeleteCourseEndpoint> {
+  public DeleteCourseEndpointSummary() {
+    Summary = "Перманентно удаляет курс";
+    Description = CanBeUsedBy.AtLeastTutor;
+    Response<Result<BaseCourseDto>>(200, "Курс успешно удалён");
+    Response<Result<ErrorResponse>>(400, "Ошибка валидации");
+    Response<Result<ErrorResponse>>(401, "Неавторизованный доступ");
+    Response<Result<ErrorResponse>>(403, "Доступ запрещён");
+  }
+}
