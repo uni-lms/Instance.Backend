817717c15cef922cb43ab9ade816d87cb5d89305
Daniil <dadyarri@gmail.com>
2024-02-29 09:41:46 +0300

    add file and description to assignment

---
+++ Aip.Instance.Backend/Data/Migrations/20240229063901_AddStaticFileAndDescriptionToAssignment.cs
@@ -0,0 +1,58 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+#nullable disable
+
+namespace Aip.Instance.Backend.Data.Migrations
+{
+    /// <inheritdoc />
+    public partial class AddStaticFileAndDescriptionToAssignment : Migration
+    {
+        /// <inheritdoc />
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<string>(
+                name: "Description",
+                table: "Assignments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.AddColumn<string>(
+                name: "FileId",
+                table: "Assignments",
+                type: "text",
+                nullable: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Assignments_FileId",
+                table: "Assignments",
+                column: "FileId");
+
+            migrationBuilder.AddForeignKey(
+                name: "FK_Assignments_StaticFiles_FileId",
+                table: "Assignments",
+                column: "FileId",
+                principalTable: "StaticFiles",
+                principalColumn: "Id");
+        }
+
+        /// <inheritdoc />
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropForeignKey(
+                name: "FK_Assignments_StaticFiles_FileId",
+                table: "Assignments");
+
+            migrationBuilder.DropIndex(
+                name: "IX_Assignments_FileId",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "Description",
+                table: "Assignments");
+
+            migrationBuilder.DropColumn(
+                name: "FileId",
+                table: "Assignments");
+        }
+    }
+}
--- Aip.Instance.Backend/Data/Models/Assignment.cs
+++ Aip.Instance.Backend/Data/Models/Assignment.cs
@@ -6,4 +6,6 @@ namespace Aip.Instance.Backend.Data.Models;
 public class Assignment : BaseContent {
   public string Title { get; set; }
   public DateTime Deadline { get; set; }
+  public StaticFile? File { get; set; }
+  public string? Description { get; set; }
 }
