9cfc905787aecff1650ecb7c585f2020fd9002a7
Daniil <dadyarri@gmail.com>
2024-02-29 12:08:30 +0300

    add endpoint to create assignment

---
+++ Aip.Instance.Backend/Api/Content/Assignment/Data/CreateAssignmentRequest.cs
@@ -0,0 +1,11 @@
+﻿namespace Aip.Instance.Backend.Api.Content.Assignment.Data;
+
+public class CreateAssignmentRequest {
+  public required string Title { get; set; }
+  public string? Description { get; set; }
+  public Guid InternshipId { get; set; }
+  public Guid SectionId { get; set; }
+  public DateTime Deadline { get; set; }
+  public bool IsVisibleToInterns { get; set; }
+  public IFormFile? File { get; set; }
+}
---
+++ Aip.Instance.Backend/Api/Content/Assignment/Services/AssignmentService.cs
@@ -0,0 +1,71 @@
+﻿using Aip.Instance.Backend.Api.Common.Services;
+using Aip.Instance.Backend.Api.Content.Assignment.Data;
+using Aip.Instance.Backend.Data;
+using Aip.Instance.Backend.Data.Common;
+using Aip.Instance.Backend.Data.Models;
+
+using Ardalis.Result;
+
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Aip.Instance.Backend.Api.Content.Assignment.Services;
+
+public class AssignmentService(AppDbContext db, StaticFileService service) {
+  public async Task<Result<SearchByIdModel>> CreateAssignment(CreateAssignmentRequest req, CancellationToken ct) {
+    var internship = await db.Internships.Where(e => e.Id == req.InternshipId).FirstOrDefaultAsync(ct);
+
+    if (internship is null) {
+      return Result.NotFound(nameof(internship));
+    }
+
+    var section = await db.Sections.Where(e => e.Id == req.SectionId).FirstOrDefaultAsync(ct);
+
+    if (section is null) {
+      return Result.NotFound(nameof(internship));
+    }
+
+    StaticFile? existedFile = null;
+
+    if (req.File is not null) {
+      var checksum = await service.CalculateChecksumAsync(req.File, ct);
+
+      existedFile = await db.StaticFiles.Where(e => e.Checksum == checksum).FirstOrDefaultAsync(ct);
+
+      if (existedFile is null) {
+        var ioResult = await service.SaveFileAsync(req.File, ct);
+
+        if (ioResult.IsSuccess) {
+          existedFile = new StaticFile {
+            Id = ioResult.FileId!,
+            Checksum = checksum,
+            Filename = req.File.FileName,
+            Filepath = ioResult.FilePath,
+          };
+
+          await db.StaticFiles.AddAsync(existedFile, ct);
+        }
+        else {
+          return Result.Error($"Не вышло загрузить файл: {ioResult.Error}");
+        }
+      }
+    }
+
+    var assignment = new Backend.Data.Models.Assignment {
+      Title = req.Title,
+      Deadline = req.Deadline,
+      Internship = internship,
+      Section = section,
+      IsVisibleToStudents = req.IsVisibleToInterns,
+      Description = req.Description,
+      File = existedFile,
+    };
+
+    await db.Assignments.AddAsync(assignment, ct);
+    await db.SaveChangesAsync(ct);
+
+    return Result.Success(new SearchByIdModel {
+      Id = assignment.Id,
+    });
+  }
+}
