ae6e4e63b9bd162b737e2eba01961550bd33ca3f
Daniil <dadyarri@gmail.com>
2024-02-29 10:49:59 +0300

    rewrite tests to use new service with fake deps

--- Aip.Instance.Backend.Tests.Unit/BytesToStringTests.cs
+++ Aip.Instance.Backend.Tests.Unit/BytesToStringTests.cs
@@ -1,6 +1,11 @@
-﻿using Aip.Instance.Backend.Api.Content.File.Services;
+﻿using Aip.Instance.Backend.Api.Common.Services;
+
+using FakeItEasy;
 
 using FluentAssertions;
+
+using Microsoft.Extensions.Hosting;
+using Microsoft.Extensions.Logging;
 
 
 namespace Aip.Instance.Backend.Tests.Unit;
@@ -15,13 +20,21 @@ public class BytesToStringTests {
     { 10240L, "10 КБ" },
     { 10490000L, "10 МБ" },
     { 1074000000L, "1 ГБ" },
+    { 10740000000L, "10 ГБ" },
   };
 
   [Theory]
   [MemberData(nameof(Cases))]
   public void WhenBytes_ReturnStringRepr(long bytes, string expected) {
-    var result = ContentFileService.BytesToString(bytes);
+    // Arrange
+    var env = A.Fake<IHostEnvironment>();
+    var logger = A.Fake<ILogger<StaticFileService>>();
+    var service = new StaticFileService(env, logger);
 
+    // Act
+    var result = service.BytesToString(bytes);
+
+    // Assert
     result.Should().Be(expected);
   }
 }
