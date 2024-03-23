321950c2d82ad071f40bfdbf6ac64a4b95c06de5
Daniil <dadyarri@gmail.com>
2024-03-23 15:40:24 +0300

    add downloading raw static file by its id

---
+++ Aip.Instance.Backend/Api/File/Data/SearchByFileId.cs
@@ -0,0 +1,5 @@
+﻿namespace Aip.Instance.Backend.Api.File.Data;
+
+public class SearchByFileId {
+  public required string Id { get; set; }
+}
---
+++ Aip.Instance.Backend/Api/File/Endpoints/DownloadFileEndpoint.cs
@@ -0,0 +1,43 @@
+﻿using Aip.Instance.Backend.Api.File.Data;
+using Aip.Instance.Backend.Configuration.Swagger;
+using Aip.Instance.Backend.Data;
+using Aip.Instance.Backend.Extensions;
+
+using Ardalis.Result;
+
+using FastEndpoints;
+
+using Microsoft.AspNetCore.StaticFiles;
+using Microsoft.EntityFrameworkCore;
+
+
+namespace Aip.Instance.Backend.Api.File.Endpoints;
+
+public class DownloadFileEndpoint(AppDbContext db) : Endpoint<SearchByFileId> {
+  public override void Configure() {
+    Version(2);
+    Post("/file/{id}/download");
+    Options(x => x.WithTags(ApiTags.FileContent.Tag));
+    DontThrowIfValidationFails();
+  }
+
+  public override async Task HandleAsync(SearchByFileId req, CancellationToken ct) {
+    var content = await db.StaticFiles
+      .Where(e => e.Id == req.Id)
+      .FirstOrDefaultAsync(ct);
+
+    if (content is null) {
+      await this.SendResponseAsync(Result.NotFound("Файл не был найден"), ct);
+      return;
+    }
+
+    var filepath = content.Filepath;
+    if (System.IO.File.Exists(filepath!)) {
+      var fileStream = new FileStream(filepath!, FileMode.Open);
+
+      new FileExtensionContentTypeProvider().TryGetContentType(content.Filepath!, out var contentType);
+      await SendStreamAsync(fileStream, fileName: content.Filename, fileLengthBytes: fileStream.Length,
+        contentType: contentType!, cancellation: ct);
+    }
+  }
+}
