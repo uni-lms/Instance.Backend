5ffc4cacf2e810e1b32da6a0921f0f799d99711d
Daniil <dadyarri@gmail.com>
2024-03-23 13:03:27 +0300

    fix response in one endpoint

--- Aip.Instance.Backend/Api/Content/File/Endpoints/Get/GetFileContentInfoEndpointSummary.cs
+++ Aip.Instance.Backend/Api/Content/File/Endpoints/Get/GetFileContentInfoEndpointSummary.cs
@@ -1,9 +1,10 @@
-﻿using Aip.Instance.Backend.Api.Content.File.Data;
-using Aip.Instance.Backend.Configuration.Swagger;
+﻿using Aip.Instance.Backend.Configuration.Swagger;
 
 using Ardalis.Result;
 
 using FastEndpoints;
+
+using FileInfo = Aip.Instance.Backend.Api.Content.File.Data.FileInfo;
 
 
 namespace Aip.Instance.Backend.Api.Content.File.Endpoints.Get;
@@ -12,7 +13,7 @@ public class GetFileContentInfoEndpointSummary : Summary<GetFileContentInfoEndpo
   public GetFileContentInfoEndpointSummary() {
     Summary = "Получает информацию о файловом контенте";
     Description = CanBeUsedBy.AnyAuthorized;
-    Response<Result<UploadFileContentResponse>>(200, "Получена информация о файловом контенте");
+    Response<Result<FileInfo>>(200, "Получена информация о файловом контенте");
     Response<Result<ErrorResponse>>(401, "Неавторизованный доступ");
     Response<Result<ErrorResponse>>(404, "Файл не найден");
   }
